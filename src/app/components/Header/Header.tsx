d05de292ddde42d32e817dd2ec4f6847b21f4380
Bryan Herrera <bryan.herrera72@gmail.com>
2026-08-27 13:57:26 -0700

    adding the summary detail window. some layout changes for the full about me section

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 import NavItems from "../NavItems/NavItems";
 function Header(): ReactNode {
   return (
-    <nav className="m-4 flex flex-row justify-between">
-      <div>I am header left content</div>
+    <nav className="m-5 pl-5 pr-5 flex flex-row justify-between font-medium">
+      <div>header left content</div>
       <div className="justify-self-end">
         <NavItems />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,102 @@
-import {Geist_Pixel, Pixelify_Sans } from "next/font/google";
-
+import { Geist_Pixel } from "next/font/google";
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa6";
 const geistPixel = Geist_Pixel({
   subsets: ["latin"],
-  variable: "--font-geist-pixel"
-})
+  variable: "--font-geist-pixel",
+});
 
 //Note: See design. Likely my landing page with the light descriptor.
 export default function Page() {
   return (
     <>
-      {/* TODO: REMOVE BORDERS */}
-      <div className="flex flex-col flex-1 m-8 p-20 gap-5 max-h-120  border-1 border-green-500">
-        <div className = "text-2xl">Hello, I'm</div>
-        <div className = {`text-6xl ${geistPixel.className} tracking-wide`}>Bryan Herrera_</div>
-        <div className = "text-xl">Software Engineer who builds scalable systems and delightful user experiences.</div>
-        <div>View my work (container here) </div>
+      {/* TOP CONTAINER */}
+      <div className="flex flex-row min-h-full">
+        {/* PRIMARY TEXT SECTION */}
+        <div className="flex flex-col flex-1 m-8 p-15 gap-5 max-h-120  ">
+          <div className="text-highlight-font text-2xl">Hello, I'm</div>
+          <div className={`text-6xl ${geistPixel.className} tracking-wide`}>
+            Bryan Herrera_
+          </div>
+          <div className="text-xl text-highlight-font">
+            Software Engineer who builds scalable systems and delightful user
+            experiences.
+          </div>
+          <div className="text-highlight-font">
+            View my work (container here){">"}
+          </div>
+        </div>
+        {/* SUMMARY DETAIL WINDOW */}
+        <div className="flex flex-col justify-between flex-1 m-8 max-h-120 border border-primary-green rounded-sm">
+          {/* Summary Detail header */}
+          <div className="flex justify-between items-center pl-3 pr-3 pt-1 pb-1 border-b border-b-primary-green">
+            {" "}
+            <span className="flex gap-2">
+              <span className="rounded-xl w-3 h-3 bg-red-400"></span>
+              <span className="rounded-xl w-3 h-3 bg-yellow-400"></span>
+              <span className="rounded-xl w-3 h-3 bg-green-400"></span>
+            </span>
+            <span>snapshot-info</span>
+          </div>
+          {/* Summary detail window details */}
+          <div className="flex flex-row p-5 gap-2 text-l">
+            <div className="flex flex-col gap-1">
+              <span>{"> "}location: </span>
+              <span>{"> "}experience: </span>
+              <span>{"> "}status: </span>
+              <span>{"> "}focus: </span>
+            </div>
+            <div className="flex flex-col gap-1">
+              <span>los angeles, ca</span>
+              <span>5+ years</span>
+              <span>open to work</span>
+              <span>full stack web development</span>
+            </div>
+          </div>
+          {/* Bottom quick links */}
+          <div className="flex flex-row justify-start gap-4 p-5 border-t border-t-primary-green">
+            {" "}
+            <span>
+              <a
+                href="https://github.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub
+                  size={24}
+                  className="hover:text-gray-600 transition-colors"
+                />
+              </a>
+            </span>
+            <span>
+              <a
+                href="bryan.herrera72@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaEnvelope
+                  size={24}
+                  className="hover:text-gray-600 transition-colors"
+                />
+              </a>
+            </span>
+            <span>
+              <a
+                href="bryan.herrera72@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin
+                  size={24}
+                  className="hover:text-gray-600 transition-colors"
+                />
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col items-center flex-1 m-8 max-h-120 border-1 border-green-500">
-        <div>RIGHT CONTENT looks like a small desktop window on this side.</div>
-      </div>
+
+      {/* DETAILED ABOUT ME SECTION */}
+      <div className="text-highlight-font text-2xl">About Me</div>
     </>
   );
 }
